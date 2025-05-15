730869b55dcf73a46b8ed2ee17318746c39cc80c
KayckRossi <kayck.12gabriel@gmail.com>
2025-05-15 16:58:51 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Musica.cs
@@ -0,0 +1,35 @@
+﻿class Musica
+{
+    public Musica(Banda artista) 
+    {
+        Artista = artista;
+    }
+
+    public string Nome { get; set; }
+    public Banda Artista { get; }
+    public int Duracao { get; set; }
+    public bool Disponivel {  get; set; }
+
+    //uma propriedade, criada como lambda
+
+    public string DescricaoResumida => $"A música {Nome} pertence a banda {Artista}";
+
+   
+    //criação do metodo/comportamentos da classe
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista:{Artista}");
+        Console.WriteLine($"Duração:{Duracao}");
+
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+
+}
---
+++ ScreenSound/Program.cs
@@ -0,0 +1,20 @@
+﻿Banda baroes = new Banda("Barões da Pisadinha");
+baroes.Nome = "dda";
+
+Album album = new Album();
+album.Nome = "O Maior Piseiro do Mundo";
+
+Musica musica1 = new Musica(baroes);
+musica1.Nome = "Volta Pra Mim";
+musica1.Duracao = 210;
+
+Musica musica2 = new Musica(baroes);
+musica2.Nome = "Chamar Eu Bebê";
+musica2.Duracao = 310;
+
+album.AdicionarMusica(musica1);
+album.AdicionarMusica(musica2);
+
+
+baroes.AdicionarAlbum(album); 
+baroes.ExibirDiscografia();
